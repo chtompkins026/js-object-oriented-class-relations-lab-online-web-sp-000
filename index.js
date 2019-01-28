50e4a697ff54789a4ec2cb65a67b205045742625
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-28 17:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,10 @@ class Driver {
     return this.trips().map(trip => {
       return trip.passenger();
     });
-    
   }
 }
 
-class Passenger { 
+class Passenger {
   constructor(name){
     this.name = name;
     this.id = ++passengerId;
@@ -31,6 +30,34 @@ class Passenger {
   trips(){
     return store.trips.filter(trip => {
       return trip.passengerId === this.id
+    })
+  }
+  drivers(){
+    return this.trips().map(trip => {
+      return trip.driver();
+    })
+  }
+}
+
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId;
+    store.trips.push(this);
+    if(driver){
+      this.driverId = driver.id;
+    }
+    if(passenger){
+      this.passengerId = passenger.id;
+    }
+  }
+  passenger(){
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    })
+  }
+  driver(){
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
     })
   }
 }
